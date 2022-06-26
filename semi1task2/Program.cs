1461b0edbb2865bba72549b0ad971227ce683234
Yuriy Agapov <nitros1989@gmail.com>
2022-06-27 00:17:42 +1000

    2

---
+++ semi1task2/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Эта программа которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.");
+Console.WriteLine("Введите первое число и нажмите Enter.");
+
+int first = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число и нажмите Enter.");
+int second = Convert.ToInt32(Console.ReadLine());
+
+if (first > second)
+{
+    Console.WriteLine($"Число {first} большее, а число {second} меньшее.");
+}
+
+else
+{
+    Console.WriteLine($"Число {second} большее, а число {first} меньшее.");
+}
+
