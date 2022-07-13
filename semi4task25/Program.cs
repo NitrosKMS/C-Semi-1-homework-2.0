44e33f71e36fd81104d4071f57c7ecb910e8cf4e
Yuriy Agapov <nitros1989@gmail.com>
2022-07-13 16:57:03 +1000

    25

---
+++ semi4task25/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+// pow = Math.Pow(A,B);
+Console.WriteLine("Эта программа которая принимает на вход два числа (A и B) и возводит число A в натуральную степень B.");
+
+Console.WriteLine("Введите число А.");
+
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите степень В.");
+
+int B = Convert.ToInt32(Console.ReadLine());
+
+int pow(int A, int B)
+{
+    int res = 1;
+    for(int i = 1; i <= B; i++)
+{
+    res = res * A;
+}    
+return res;
+}
+
+long result = pow(A, B);
+
+Console.WriteLine($"Число {A} в степени {B} -> {result}");
