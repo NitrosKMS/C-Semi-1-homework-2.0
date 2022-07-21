49f01d87bf23af7573fc7f532c965f66d9a8873d
Yuriy Agapov <nitros1989@gmail.com>
2022-07-21 23:26:02 +1000

    34

---
+++ semi5task34/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+
+Console.WriteLine($"Эта программа которая показывает количество чётных чисел в массиве.");
+Console.WriteLine("Введите количество чисел в массиве.");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int[] numbers = new int[num];
+
+void FillArray(int[] array, int min, int max)
+
+{
+  for (int i = 0; i < array.Length; i++ ){
+    array[i] = new Random().Next(min, max);
+  }
+}
+
+void WriteArray(int[] array)
+
+{
+    for (int i = 0; i < array.Length; i++ ){
+    Console.Write(array[i] + " ");
+  }
+  Console.WriteLine();
+}
+
+int QuantityPositive(int[] array)
+
+{
+    int even = 0;
+    for (int i = 0; i < array.Length; i++ ){
+    if (array[i] % 2 == 0)
+    {
+      even++;
+    }
+  }
+  return even;
+}
+
+FillArray(numbers, 100, 999);
+WriteArray(numbers);
+Console.WriteLine();
+
+int even = QuantityPositive(numbers);
+Console.WriteLine($"Количество чётных чисел в массиве: {even}");
