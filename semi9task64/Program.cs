bef058033ef7cf7d0af180774d244a3a1867a2df
Yuriy Agapov <nitros1989@gmail.com>
2022-08-05 11:39:11 +1000

    64

---
+++ semi9task64/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.WriteLine($"Это программа, которая выведет все натуральные числа в промежутке от N до 1.");
+int num = InputNumbers("Введите N: ");
+int count = 2;
+PrintNumber(num, count);
+Console.Write(1);
+
+void PrintNumber(int num, int count)
+{
+  if (count > num) return;
+  PrintNumber(num, count + 1);
+  Console.Write(count + ", ");
+}
+
+int InputNumbers(string input) 
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
